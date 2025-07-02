1232a5870bcdb47d37d965bbe9c27c62c39c9eaf
Fabrielg <gfrancois2003@gmail.com>
2025-07-02 16:40:18 +0200

    refactor: apply colors to print state messages

--- print_state.c
+++ print_state.c
@@ -6,11 +6,18 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 14:05:47 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/07/02 16:08:56 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/07/02 16:38:40 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+#define LEFT_FORK "[%-6ld]"BLACK" %d has taken the left fork\n"RESET
+#define RIGHT_FORK "[%-6ld]"BLACK" %d has taken the right fork\n"RESET
+#define IS_EATING "[%-6ld]"WHITE" %d is eating\n"RESET
+#define IS_SLEEPING "[%-6ld]"BLUE" %d is sleeping\n"RESET
+#define IS_THINKING "[%-6ld]"CYAN" %d is thinking\n"RESET
+#define IS_DEAD "[%-6ld]"RED" %d died\n"RESET
 
 void	print_state(t_state state, t_philo *philo)
 {
@@ -19,16 +26,16 @@ void	print_state(t_state state, t_philo *philo)
 	elapsed = get_time(MILLISECOND);
 	mutex_op(&philo->table->print, LOCK);
 	if (state == TAKE_LEFT_FORK && !is_simulation_finished(philo->table))
-		printf("%-6ld %d has taken the left fork\n", elapsed, philo->id);
+		printf(RIGHT_FORK, elapsed, philo->id);
 	else if (state == TAKE_RIGHT_FORK && !is_simulation_finished(philo->table))
-		printf("%-6ld %d has taken the right fork\n", elapsed, philo->id);
+		printf(LEFT_FORK, elapsed, philo->id);
 	else if (state == EATING && !is_simulation_finished(philo->table))
-		printf("%-6ld %d is eating\n", elapsed, philo->id);
+		printf(IS_EATING, elapsed, philo->id);
 	else if (state == SLEEPING && !is_simulation_finished(philo->table))
-		printf("%-6ld %d is sleeping\n", elapsed, philo->id);
+		printf(IS_SLEEPING, elapsed, philo->id);
 	else if (state == THINKING && !is_simulation_finished(philo->table))
-		printf("%-6ld %d is thinking\n", elapsed, philo->id);
+		printf(IS_THINKING, elapsed, philo->id);
 	else if (state == DIED && !is_simulation_finished(philo->table))
-		printf("%-6ld %d died\n", elapsed, philo->id);
+		printf(IS_DEAD, elapsed, philo->id);
 	mutex_op(&philo->table->print, UNLOCK);
 }
