b1f3d979a9874bf2fb0e548c170bb8d4e6b18f10
Fabrielg <gfrancois2003@gmail.com>
2025-07-03 15:14:15 +0200

    fix: display elapsed time instead of get_time

--- print_state.c
+++ print_state.c
@@ -6,31 +6,31 @@
 /*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 14:05:47 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/07/03 15:04:10 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/07/03 15:12:07 by gfrancoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-#define LEFT_FORK "[%-6ld]"BLACK" %d takes the left fork\n"RESET
-#define RIGHT_FORK "[%-6ld]"BLACK" %d takes the right fork\n"RESET
-#define IS_EATING "[%-6ld]"WHITE" %d is eating\n"RESET
-#define IS_SLEEPING "[%-6ld]"BLUE" %d is sleeping\n"RESET
-#define IS_THINKING "[%-6ld]"CYAN" %d is thinking\n"RESET
-#define IS_DEAD "[%-6ld]"RED" %d died\n"RESET
+#define LEFT_FORK "[%6ld]"BLACK" %d takes the left fork\n"RESET
+#define RIGHT_FORK "[%6ld]"BLACK" %d takes the right fork\n"RESET
+#define IS_EATING "[%6ld]"WHITE" %d is eating\n"RESET
+#define IS_SLEEPING "[%6ld]"BLUE" %d is sleeping\n"RESET
+#define IS_THINKING "[%6ld]"CYAN" %d is thinking\n"RESET
+#define IS_DEAD "[%6ld]"RED" %d died\n"RESET
 
-#define D_LEFT_FORK "[%-6ld]"BLACK" %d takes the left fork	[🍴 - %d]\n"RESET
-#define D_RIGHT_FORK "[%-6ld]"BLACK" %d takes the right fork\t[%d - 🍴]\n"RESET
-#define D_IS_EATING "[%-6ld]"WHITE" %d is eating		[🍝 %d]\n"RESET
-#define D_IS_SLEEPING "[%-6ld]"BLUE" %d is sleeping		[💤]\n"RESET
-#define D_IS_THINKING "[%-6ld]"CYAN" %d is thinking		[🤔]\n"RESET
-#define D_IS_DEAD "[%-6ld]"RED" %d died	[💀]\n"RESET
+#define D_LEFT_FORK "[%6ld]"BLACK" %d takes the left fork	[🍴 - %d]\n"RESET
+#define D_RIGHT_FORK "[%6ld]"BLACK" %d takes the right fork\t[%d - 🍴]\n"RESET
+#define D_IS_EATING "[%6ld]"WHITE" %d is eating		[🍝 %d]\n"RESET
+#define D_IS_SLEEPING "[%6ld]"BLUE" %d is sleeping		[💤]\n"RESET
+#define D_IS_THINKING "[%6ld]"CYAN" %d is thinking		[🤔]\n"RESET
+#define D_IS_DEAD "[%6ld]"RED" %d died	[💀]\n"RESET
 
 void	print_state(t_state state, t_philo *philo)
 {
 	long	elapsed;
 
-	elapsed = get_time(MILLISECOND);
+	elapsed = get_time(MILLISECOND) - philo->table->start_simulation;
 	if (DEBUG_MODE)
 		return (print_state_debug(state, philo, elapsed));
 	mutex_op(&philo->table->print, LOCK);
