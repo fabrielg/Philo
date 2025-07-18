bccf23c340147cc61976a327379e3117916568eb
Fabrielg <gfrancois2003@gmail.com>
2025-07-18 18:39:58 +0200

    style: remove colors when debug mode is false

--- print_state.c
+++ print_state.c
@@ -12,12 +12,12 @@
 
 #include "philo.h"
 
-#define LEFT_FORK "%ld %s%d has taken a fork\n"
-#define RIGHT_FORK "%ld %s%d has taken a fork\n"
-#define IS_EATING "%ld %s%d is eating\n"
-#define IS_SLEEPING "%ld %s%d is sleeping\n"
-#define IS_THINKING "%ld %s%d is thinking\n"
-#define IS_DEAD "%ld %s%d died\n"
+#define LEFT_FORK "%ld %d has taken a fork\n"
+#define RIGHT_FORK "%ld %d has taken a fork\n"
+#define IS_EATING "%ld %d is eating\n"
+#define IS_SLEEPING "%ld %d is sleeping\n"
+#define IS_THINKING "%ld %d is thinking\n"
+#define IS_DEAD "%ld %d died\n"
 
 #define D_LEFT_FORK "[%6ld] %s%d takes the left fork	[🍴 - %d]\n"
 #define D_RIGHT_FORK "[%6ld] %s%d takes the right fork\t[%d - 🍴]\n"
@@ -54,17 +54,16 @@ void	print_state(t_state state, t_philo *philo)
 		return (print_state_debug(state, philo, elapsed));
 	mutex_op(&philo->table->print, LOCK);
 	if (state == TAKE_LEFT_FORK && !is_simulation_finished(philo->table))
-		printf(LEFT_FORK, elapsed, BLACK, philo->id);
+		printf(LEFT_FORK, elapsed, philo->id);
 	else if (state == TAKE_RIGHT_FORK && !is_simulation_finished(philo->table))
-		printf(RIGHT_FORK, elapsed, BLACK, philo->id);
+		printf(RIGHT_FORK, elapsed, philo->id);
 	else if (state == EATING && !is_simulation_finished(philo->table))
-		printf(IS_EATING, elapsed, WHITE, philo->id);
+		printf(IS_EATING, elapsed, philo->id);
 	else if (state == SLEEPING && !is_simulation_finished(philo->table))
-		printf(IS_SLEEPING, elapsed, BLUE, philo->id);
+		printf(IS_SLEEPING, elapsed, philo->id);
 	else if (state == THINKING && !is_simulation_finished(philo->table))
-		printf(IS_THINKING, elapsed, CYAN, philo->id);
+		printf(IS_THINKING, elapsed, philo->id);
 	else if (state == DIED && !is_simulation_finished(philo->table))
-		printf(IS_DEAD, elapsed, RED, philo->id);
-	printf("%s", RESET);
+		printf(IS_DEAD, elapsed, philo->id);
 	mutex_op(&philo->table->print, UNLOCK);
 }
